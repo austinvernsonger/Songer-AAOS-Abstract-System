b02d1be41eafafed7f22d6044c05e1049967a53f
Austin Vern Songer <austinvernsonger@outlook.com>
2016-09-07 15:23:31 -0500

    1

--- includes/songer-admin-theme/index.php
+++ includes/songer-admin-theme/index.php
@@ -7,14 +7,14 @@
 function get_ba_options($option)
 {
 	$options= array(
-					'name'=>"Blue Admin",
-					'slug'=>"blue_admin", 
+					'name'=>"Songer Admin",
+					'slug'=>"songer_admin", 
 					'version'=>'16.07.16', 
 					'url'=> plugin_dir_url(__FILE__),
 					'path'=> plugin_dir_path(__FILE__),
 					'settings'=>array (	
 	
-							 		'blue_admin_adminbar'=>array(  
+							 		'songer_admin_adminbar'=>array(  
 										  "name" => "Custom Adminbar Menus",
 										  "desc" => "Enable or disable custom Adminbar menus. You can add your own links to WordPress adminbar or toolbar",
 										  "default" => false, 	
@@ -23,7 +23,7 @@ function get_ba_options($option)
 										  "settings_page"=>false,  
 										  "learn_more"=>'http://bit.ly/1rCaAzf'
 									  ),
-									'blue_admin_login_page'=> array(  
+									'songer_admin_login_page'=> array(  
 										  "name" => 'Login Page',
 										  "desc" => "You can costomize WordPress login page style here.",
 										  "default" => false, 	
@@ -32,7 +32,7 @@ function get_ba_options($option)
 										  "settings_page"=>true, 
 										  "learn_more"=>'http://bit.ly/1Wi8iC0'
 									  ),
-									 'blue_admin_color_scheme'=>array(  
+									 'songer_admin_color_scheme'=>array(  
 										  "name" =>'Color Schemes',
 										  "desc" => "Enable or disable Blue Admin color schemes.",
 										  "default" => false, 	
@@ -42,7 +42,7 @@ function get_ba_options($option)
 										  "learn_more"=>''
 									  ),
 									  
-									   'blue_admin_color_scheme_val'=>array(  
+									   'songer_admin_color_scheme_val'=>array(  
 									   	 	"default"=>'',
 										  "settings_type"=>'',
 										  "settings_page"=>false,  
